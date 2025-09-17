d335c91ab3179fe9e2737c6219c9fef26538be98
Ramir-C <163381802+Ramir-C@users.noreply.github.com>
2025-09-16 18:47:40 -0600

    Update MySQL connection environment variables

--- db.js
+++ db.js
@@ -1,19 +1,21 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306,
 });
 
 connection.connect((err) => {
   if (err) {
     console.error("❌ Error al conectar a MySQL:", err);
-    return;
+    process.exit(1);
+  } else {
+    console.log("✅ Conectado a la base de datos MySQL");
   }
-  console.log("✅ Conectado a MySQL en Railway 🚀");
 });
 
 module.exports = connection;
