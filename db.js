e9033e660fd19d24a202579b384d9c4d847e00f6
Ramir-C <163381802+Ramir-C@users.noreply.github.com>
2025-09-16 18:35:30 -0600

    Add MySQL connection setup in db.js

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("❌ Error al conectar a MySQL:", err);
+    return;
+  }
+  console.log("✅ Conectado a MySQL en Railway 🚀");
+});
+
+module.exports = connection;
